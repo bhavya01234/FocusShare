aac09f3203911561f91c0988b848894aff63bde9
madhurajituri <jiturimadhura@gmail.com>
2024-04-18 12:57:32 +0530

    login page frontend done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,8 @@
-// import React from 'react'
-// import "./App.css";
-// import { BrowserRouter as Router } from "react-router-dom";
-// // import { Toaster } from 'react-hot-toast';
-
-// // import Login from './components/Login/Login';
-// // import Signup from './components/Signup/Signup';
-// const App = () => {
-//   return (
-//     <Router>
-//       {/* <Login/>
-//       <Signup/> */}
-//     </Router>
-//   );
-// }
-
-// export default App
-
-
-// import './App.css'
 import Home from "./component/Home"
 import Login from "./component/Login"
 import Signup from "./component/Signup"
 import Room from "./component/Room";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { useState } from 'react';
 import JoinRoom from "./component/Join"
 
 
--- frontend/src/component/Login.jsx
+++ frontend/src/component/Login.jsx
@@ -1,19 +1,16 @@
-
-
-
-
 import React, { useEffect, useState } from "react"
 import axios from "axios"
 import { useNavigate, Link } from "react-router-dom"
+import student from '../images/student.avif'
 // import "./Signup.css"
 
 function Login() {
 
-    const history=useNavigate();
+    const history = useNavigate();
 
-    const [username,setUsername]=useState('')
-    const [email,setEmail]=useState('')
-    const [password,setPassword]=useState('')
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     // async function submit(e){
     //     e.preventDefault();
@@ -58,59 +55,74 @@ function Login() {
     //     }
 
     // }
-    async function submit(e){
-    e.preventDefault();
+    async function submit(e) {
+        e.preventDefault();
 
-    try{
-        const response = await axios.post("http://localhost:8001/users/login", {
-            email,
-            username,
-            password
-        });
+        try {
+            const response = await axios.post("http://localhost:8001/users/login", {
+                email,
+                username,
+                password
+            });
 
-        console.log("Response from backend:", response); // Check the entire response object
+            console.log("Response from backend:", response); // Check the entire response object
 
-        if (response.status === 200) {
-            console.log("responseeee data: ", response.data);
-            const { data } = response.data; // Access the 'data' object from the response
-            const { accessToken } = data; // Extract the 'accessToken' from the 'data' object
-            console.log("Access token before setting:", accessToken);
-            localStorage.setItem('token', accessToken);
-            console.log("Access token after setting:", localStorage.getItem('token'));
-            history("/joinroom", { state: { id: email } });
-        } else if (response.data === "notexist") {
-            alert("User has not signed up");
-        } else {
-            alert("Login failed. Please try again.");
+            if (response.status === 200) {
+                console.log("responseeee data: ", response.data);
+                const { data } = response.data; // Access the 'data' object from the response
+                const { accessToken } = data; // Extract the 'accessToken' from the 'data' object
+                console.log("Access token before setting:", accessToken);
+                localStorage.setItem('token', accessToken);
+                console.log("Access token after setting:", localStorage.getItem('token'));
+                history("/joinroom", { state: { id: email } });
+            } else if (response.data === "notexist") {
+                alert("User has not signed up");
+            } else {
+                alert("Login failed. Please try again.");
+            }
+        } catch (e) {
+            console.error("Error:", e);
+            alert("An error occurred. Please try again later.");
         }
-    } catch(e) {
-        console.error("Error:", e);
-        alert("An error occurred. Please try again later.");
     }
-}
 
 
 
     return (
-        <div className="container">
+        <>
+            <div className="h-screen w-full bg-gradient-to-r from-pink-200 to-purple-900 via-purple-700 flex">
+                <div className="m-20 w-full bg-white/30 rounded-xl flex justify-between items-center px-20">
+                    <div className="flex flex-col gap-4">
+                        <div className="text-5xl text-purple-900 tracking-tighter font-bold">Focus Share ...</div>
+                        <img src={student} className="rounded-full" />
+                    </div>
+                    <div className="mr-12">
+                        <div className="p-4 rounded-xl text-white flex items-center justify-center flex-col gap-4">
+                            <h1 className="text-4xl font-bold text-purple-900">Login</h1>
+                            <form action="POST" className="flex flex-col gap-6">
+                                <div className="flex flex-col gap-2">
+                                    <h3 className="font-semibold text-base">Enter Username</h3>
+                                    <input className="w-72 h-8 rounded-md" type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder="UserName" />
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <h3 className="font-semibold text-base">Enter Email Address</h3>
+                                    <input className="w-72 h-8 rounded-md" type="text" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email" />
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <h3 className="font-semibold text-base">Enter Password</h3>
+                                    <input className="w-72 h-8 rounded-md" type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password" />
+                                </div>
+                            </form>
+                            <input className="p-2 items-center w-20 bg-pink-400 text-white rounded-lg hover:bg-pink-200 hover:text-purple-900 shadow-lg" type="submit" onClick={submit} value="Login" />
+                            <p>OR</p>
+                            <Link to="/signup" className="underline text-xl text-purple-900 font-semibold">Signup</Link>
+                        </div>
 
-            <h1 className="heading">Login Page</h1>
+                    </div>
+                </div>
 
-            <form action="POST">
-                <input type="text" onChange={(e) => { setUsername(e.target.value) }} placeholder="UserName"  />
-                <input type="text" onChange={(e) => { setEmail(e.target.value) }} placeholder="Email"  />
-                <input type="password" onChange={(e) => { setPassword(e.target.value) }} placeholder="Password"  />
-                <input type="submit" onClick={submit} className="submitbtn" value="Login"/>
-
-            </form>
-
-          
-            <p>OR</p>
-           
-
-            <Link to="/signup" className="link">Signup Page</Link>
-
-        </div>
+            </div>
+        </>
     )
 }
 
